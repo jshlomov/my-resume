0a974ea88bfc59d3dc6d0096d2a32941b9d3d482
jshlomov <jonatanshv@gmail.com>
2023-10-19 20:37:53 +0300

    projects step 2

--- src/Components/myProjects/Projects.jsx
+++ src/Components/myProjects/Projects.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./Projects.css";
 import ProjectCard from "./ProjectCard";
 import DroneImg from "../../imgs/ProjectsImgs/drone2.png";
+import Pro2Img from "../../imgs/ProjectsImgs/project2.png";
+
 
 function Projects() {
   return (
@@ -17,13 +19,13 @@ function Projects() {
       />
 
       <ProjectCard
-        title="Real-time Drone Delivery `Management Solution (C#, .NET, WPF):"
-        description="Developed a real-time delivery system management solution using the three-layer model
-        architecture.
-        Implemented a user interface for Windows systems (WPF) and utilized language tools.
-        Incorporated important programming principles like SOLID, Design Patterns, OOP &
-        OOD, multithreading, and version control with GitHub."
-        image={DroneImg}
+        title="3D Graphic Scene Modeling Solution (Java):"
+        description="Designed and implemented a solution for creating and modeling virtual 3D graphic scenes with realistic lighting effects.
+        Covered concepts such as light sources, rays, reflections, transparencies, and other
+        advanced rendering techniques.
+        Incorporated principles of software engineering, OOP & OOD, Design Patterns, SOLID,
+        and Test-driven Development (TDD)."
+        image={Pro2Img}
       />
 
       <ProjectCard
